a0d6484f8217a4356d78f3e3f688ade372f8b448
DECRATE1 <yandovin2005@mail.ru>
2024-08-14 22:40:48 +0300

    China number one 2

--- src/componensts/MainProducts/MainProductCard/MainProductCard.tsx
+++ src/componensts/MainProducts/MainProductCard/MainProductCard.tsx
@@ -6,7 +6,8 @@ export default function MainProductCard({ img, name }: ProductsProp) {
   return (
     <div className="flex w-fit justify-center items-center flex-col border-2 border-black bg-white mb-2">
       <div
-        className={`bg-[url('${img}')] hover:bg-[url('/SG.webp')] bg-no-repeat bg-cover w-96 h-96`}
+        className={` bg-no-repeat bg-cover w-96 h-96 hover:!rbg-[url('/SG.webp')]`}
+        style={{ backgroundImage: `url(${img})` }}
       ></div>
       <div>
         <span className="text-black">{name}</span>
